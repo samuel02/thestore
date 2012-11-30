78369169e47d63e81dc51264f16fa1872905d0b4
Samuel Nilsson <samuel02@gmail.com>
2012-11-30 15:36:21 +0100

    Added support for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    admin = Admin.authenticate(params[:email], params[:password])
+    if admin
+      session[:admin_id] = admin.id
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:admin_id] = nil
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Thestore::Application.routes.draw do
   resources :products
   resources :admins, :only => [:new, :create]
 
+  get "login" => "sessions#new", :as => "login"
+  get "logout" => "sessions#destroy", :as => "logout"
+
   root :to => 'pages#index'
 end
