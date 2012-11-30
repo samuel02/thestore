1ea4eee6521de181b441fdc438d3d55c754ec956
Samuel Nilsson <samuel02@gmail.com>
2012-11-30 13:28:44 +0100

    Added simple test for index in pages controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe PagesController do
 
+  def valid_product_attributes
+    {}
+  end
+
+  def valid_session
+    {}
+  end
+
+  describe 'GET index' do
+    it 'assigns all products as @products' do
+      product = Product.create! valid_product_attributes
+      get :index, {}, valid_session
+      assigns(:products).should eq([product])
+    end
+  end
+
 end
