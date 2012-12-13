31eac0da68ad80213ef25b7c7854cd2c08d0319b
Samuel Nilsson <samuel02@gmail.com>
2012-12-13 15:28:00 +0100

    Make sure the cart is returned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
     rescue ActiveRecord::RecordNotFound
       cart = Cart.create
       session[:cart_id] = cart.id
+      return cart
     end
 
     cart
